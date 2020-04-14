417e82c878e9a3ce75d317cac619ff90d56ab984
naufalzhafran <latifnaufal7@gmail.com>
2020-04-14 17:04:35 +0700

    add kmeans and agglomerative file

---
+++ MyAgglomerative.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+
+class MyAgglomerative():
+    def __init__(self):
+        pass
+
+    def fit(self,x):
+        pass
+
+    def predict(self,x):
+        pass
--- MyKMeans.py
+++ MyKMeans.py
@@ -2,11 +2,25 @@ import numpy as np
 
 
 class MyKMeans():
-    def __init__(self):
-        pass
+    def __init__(self,K):
+        self.K = K
 
-    def fit(self,x,y):
-        pass
+    def fit(self,x):
+        self.centroids = x[np.random.choice(len(x), self.K, replace=False)]
+        self.prev_class = None
+        self.classes = np.zeros(len(x))
+        while not np.all(self.classes == self.prev_class) :
+            self.prev_class = self.classes
+            self.classes = self.predict(x)
+            self.update_centroid(x)
 
-    def predict(self,x):
-        pass
+    def predict(self, x):
+        return np.apply_along_axis(self.compute_class, 1, x)
+
+    def compute_class(self, x):
+        return np.argmin(np.sqrt(np.sum((self.centroids - x)**2, axis=1)))
+
+    def update_centroid(self, x):
+        self.centroids = np.array([np.mean(x[self.classes == k], axis=0)  for k in range(self.K)])
+
+
