c11b958cd5a420cb852df7c17fa08f50ebf6c54b
naufalzhafran <latifnaufal7@gmail.com>
2020-04-14 17:05:51 +0700

    make kmeans code simpler

--- MyKMeans.py
+++ MyKMeans.py
@@ -12,15 +12,10 @@ class MyKMeans():
         while not np.all(self.classes == self.prev_class) :
             self.prev_class = self.classes
             self.classes = self.predict(x)
-            self.update_centroid(x)
+            self.centroids = np.array([np.mean(x[self.classes == k], axis=0)  for k in range(self.K)])
 
     def predict(self, x):
         return np.apply_along_axis(self.compute_class, 1, x)
 
     def compute_class(self, x):
         return np.argmin(np.sqrt(np.sum((self.centroids - x)**2, axis=1)))
-
-    def update_centroid(self, x):
-        self.centroids = np.array([np.mean(x[self.classes == k], axis=0)  for k in range(self.K)])
-
-
